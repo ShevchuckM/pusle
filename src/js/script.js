dc42deb43ee55edc383e2fc4741133c5ff496097
Max <maxim.shevchu34@gmail.com>
2022-06-03 18:30:22 +0300

    post email

--- src/js/script.js
+++ src/js/script.js
@@ -79,5 +79,20 @@ $(document).ready(function(){
 
     $("input[name=phone]").mask("+38 (999) 999-9999");
     
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+      });
+      return false;
+    });
 
   });
